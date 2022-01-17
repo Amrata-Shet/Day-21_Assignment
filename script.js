dbe95e8fa6e7a09e8431ce6c25f0fd27517e6559
Amrata-Shet <56686812+Amrata-Shet@users.noreply.github.com>
2022-01-17 23:25:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let timer=document.getElementById("timer")
+
+let time=10;
+timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML=time--;
+setTimeout(function() {
+    timer.innerHTML="Happy Independence Day!!"
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);   
+},1000);
+},1000);
